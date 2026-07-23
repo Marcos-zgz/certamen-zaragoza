6e0b7e898657b60234ce43870b5dcc282b92397c
Marcos-zgz <mabad350@alumno.uned.es>
2026-07-23 10:43:32 +0200

    Uncomment Nav import in index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-//import Nav from "../components/nav";
+import Nav from "../components/nav";
 import Image from "next/image";
 
 export default function Home() {
